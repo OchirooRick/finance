4429c9328936bbb81881158073580c8887eb65b6
hellboy0805 <ochiroo.ochiroo0805@gmail.com>
2022-09-06 12:52:41 +0800

    Finance iin modulruu item add hiideg bolgono

--- app.js
+++ app.js
@@ -22,21 +22,22 @@ var uiController = (function () {
 })();
 
 // Санхүүтэй ажиллах Controller
+// ptivate data
 var financeController = (function () {
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-
+  // ptivate data
   var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-
+  // ptivate data
   var data = {
-    allItems: {
+    items: {
       inc: [],
       exp: [],
     },
@@ -45,14 +46,38 @@ var financeController = (function () {
       exp: 0,
     },
   };
+
+  // public serves
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+      data.items[type].push(item);
+    },
+    seeData: function () {
+      return data;
+    },
+  };
 })();
 
 // Программ холбогч Controller
 var appController = (function (uiController, fnController) {
   var ctrlAddItem = function () {
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн Controller - т дамжуулж тэнд хадгалнав
+    //financeController.addItem();
+    financeController.addItem(input.type, input.description, input.value);
     // 3. Олж авсан өгөгдлүүдийг вэб дээрээ тохирох хэсэгт нь гаргана
     // 4. Төсөвийг тооцоолно
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана
